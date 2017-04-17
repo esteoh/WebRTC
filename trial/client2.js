da00b905006bbc7fe368e8d6d568ecb16b131f23
ES Teoh <esteoh@gmail.com>
2017-04-17 15:36:42 +0800

    client example no. 2

---
+++ trial/client2.js
@@ -0,0 +1,169 @@
+var connection = new WebSocket('ws://localhost:9090'); 
+var name = "";
+
+var loginInput = document.querySelector('#loginInput');
+var loginBtn = document.querySelector('#loginBtn');
+var otherUsernameInput = document.querySelector('#otherUsernameInput'); 
+var connectToOtherUsernameBtn = document.querySelector('#connectToOtherUsernameBtn');
+var connectedUser, myConnection;
+
+//when a user clicks the login button 
+loginBtn.addEventListener("click", function(event) {
+    name = loginInput.value;
+
+    if(name.length > 0) {
+        send({
+            type: "login",
+            name: name
+        });
+    }
+});
+
+//handle messages from the server
+connection.onmessage = function (message) {
+    console.log("Got message", message.data);
+
+    // var data;
+    // try {
+    //     data = JSON.parse(message.data);
+    //     switch(data.type) { 
+    //         case "login": 
+    //             onLogin(data.success); 
+    //             break; 
+    //         case "offer": 
+    //             onOffer(data.offer, data.name); 
+    //             break; 
+    //         case "answer": 
+    //             onAnswer(data.answer); 
+    //             break; 
+    //         case "candidate": 
+    //             onCandidate(data.candidate); 
+    //             break; 
+    //         case "hello":
+    //             console.log(data.say);
+    //         default: 
+    //             break; 
+    //     }
+    // } catch (error) {
+    //     console.log(message.data);
+    // }
+
+    var data = JSON.parse(message.data); 	
+    switch(data.type) { 
+        case "login": 
+            onLogin(data.success); 
+            break; 
+        case "offer": 
+            onOffer(data.offer, data.name); 
+            break; 
+        case "answer": 
+            onAnswer(data.answer); 
+            break; 
+        case "candidate": 
+            onCandidate(data.candidate); 
+            break; 
+        // case "hello":
+        //     console.log(data.say);
+        default: 
+            break; 
+    }
+};
+
+//when a user logs in
+function onLogin(success) {
+
+    if (success === false) {
+        alert("Oops...Try a different username");
+    } else {
+        //creating our RTCPeerConnection object
+
+        var configuration = {
+            "iceServers": [{ "url": "stun:stun.1.google.com:19302" }]
+        };
+
+        // myConnection = new webkitRTCPeerConnection(configuration);
+        myConnection = new RTCPeerConnection(configuration);
+        console.log("RTCPeerConnection object was created");
+        console.log(myConnection);
+
+        //setup ice handling
+        //when the browser finds an ice candidate we send it to another peer
+        myConnection.onicecandidate = function (event) {
+
+            if (event.candidate) {
+                send({
+                    type: "candidate:",
+                    candidate: event.candidate
+                });
+            }
+        };
+    }
+};
+
+connection.onopen = function () {
+    console.log("Connected");
+};
+
+connection.onerror = function (err) {
+    console.log("Got error", err);
+};
+
+// Alias for sending messages in JSON format
+function send(message) {
+
+    if (connectedUser) {
+        message.name = connectedUser;
+    }
+
+    connection.send(JSON.stringify(message));
+};
+
+//setup a peer connection with another user
+connectToOtherUsernameBtn.addEventListener("click", function () {
+
+    var otherUsername = otherUsernameInput.value;
+    connectedUser = otherUsername;
+
+    if (otherUsername.length > 0) {
+        //make an offer
+        myConnection.createOffer(function (offer) {
+            console.log();
+            send({
+                type: "offer",
+                offer: offer
+            });
+
+            myConnection.setLocalDescription(offer);
+        },  function (error) {
+            alert("An error has occured.");
+        });
+    }
+});
+
+//when somebody want to call us
+function onOffer(offer, name) {
+    connectedUser = name;
+    myConnection.setRemoteDescription(new RTCSessionDescription(offer));
+
+    myConnection.createAnswer(function (answer) {
+        myConnection.setLocalDescription(answer);
+
+        send({
+            type: "answer",
+            answer: answer
+        });
+
+    },  function (error) {
+        alert("oops...error");
+    });
+}
+
+//when another user answer to our offer
+function onAnswer(answer) {
+    myConnection.setRemoteDescription(new RTCSessionDescription(answer));
+}
+
+//when we got ice candidate from remote user
+function onCandidate(candidate) {
+    myConnection.addIceCandidate(new RTCIceCandidate(candidate));
+}
