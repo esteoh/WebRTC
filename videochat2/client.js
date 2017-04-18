d2c53b28586aa9e917d331ead639b89d515dca75
ES Teoh <esteoh@gmail.com>
2017-04-18 12:06:29 +0800

    create client.js

--- videochat2/client.js
+++ videochat2/client.js
@@ -51,3 +51,92 @@ function send( message) {
 
     conn.send(JSON.stringify(message));
 };
+
+
+/*
+UI selectors block
+*/
+
+var loginPage = document.querySelector('#loginPage');
+var usernameInput = document.querySelector('#usernameInput');
+var loginBtn = document.querySelector('#loginBtn');
+
+var callPage = document.querySelector('#callPage');
+var callToUsernameInput = document.querySelector('#callToUsernameInput');
+var callBtn = document.querySelector('#callBtn');
+
+var hangUpBtn = document.querySelector('#hangUpBtn');
+
+var localVideo = document.querySelector('#localVideo'); 
+var remoteVideo = document.querySelector('#remoteVideo');
+ 
+var yourConn; 
+var stream;
+
+//hide call page
+callPage.style.display = "none";
+
+//login when user clicks the button
+loginBtn.addEventListener("click", function (event) {
+    name = usernameInput.value;
+
+    if (name.length > 0) {
+        send({
+            type: "login",
+            name: name
+        });
+    }
+});
+
+function handleLogin(success0) {
+
+    if (success === false) {
+        alert("Oops...Please try a different username");
+    } else {
+        //display the call page if login is successfull
+        loginPage.style.display = "none";
+        callPage.style.display = "block";
+
+        //********************** 
+        //Starting a peer connection 
+        //********************** 
+        
+        //getting local video stream
+        navigator.getUserMedia({ video: true, audio: true}, function (myStream) {
+            stream = myStream;
+
+            //display local video stream on the page
+            localVideo.src = window.URL.createObjectURL(stream);
+
+            //using Google public stun server
+            var configuration = {
+                "iceServers": [{ "url": "stun:stun2.1.google.com:19320" }]
+            };
+
+            yourConn = new RTCPeerConnection(configuration);
+
+            //setup stream listening
+            yourConn.addStream(stream);
+
+            //when a remote user adds stream to the peer connection, we display it
+            yourConn.onaddstream = function (e) {
+                remoteVideo.src = window.URL.createObjectURL(e.stream);
+            };
+
+            //setup ice handling
+            yourConn.onicecandidate = function (event) {
+
+                if (event.candidate) {
+                    send({
+                        type: "candidate",
+                        candidate: event.candidate
+                    });
+                }
+
+            };
+
+        }, function (error) {
+            console.log(error);
+        });
+    }
+};
